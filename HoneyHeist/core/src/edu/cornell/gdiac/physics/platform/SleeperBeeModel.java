e4af403744351b0caa64f0af67b8375484d29374
Justin Ngai <jn537@cornell.edu>
2021-03-26 17:41:46 -0400

    Complete enemy model and relevant asset rerouting
    - Updated the key and value for the ant, sleeperBee and chaserBee asset
    - Uploaded ant, chaserBee and sleeperBee images
    - Constructed abstract bee model and two types of bees that implement the model.

---
+++ HoneyHeist/core/src/edu/cornell/gdiac/physics/platform/SleeperBeeModel.java
@@ -0,0 +1,16 @@
+package edu.cornell.gdiac.physics.platform;
+
+import com.badlogic.gdx.utils.JsonValue;
+
+public class SleeperBeeModel extends AbstractBeeModel{
+    /**
+     * Creates a bee avatar with the given physics data
+     *
+     * @param data   The physics constants for the player Ant
+     * @param width  The object width in physics units
+     * @param height The object width in physics units
+     */
+    public SleeperBeeModel(JsonValue data, float width, float height) {
+        super(data, width, height);
+    }
+}
