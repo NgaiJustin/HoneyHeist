24d13fbecaa0e61d37bd82b65c7b71f67b6b3624
Haruhi_Aisaka <bobingbobs@protonmail.com>
2021-03-28 16:17:04 -0400

    AI Controller

--- HoneyHeist/core/src/edu/cornell/gdiac/physics/platform/AIController.java
+++ HoneyHeist/core/src/edu/cornell/gdiac/physics/platform/AIController.java
@@ -12,6 +12,7 @@ import com.badlogic.gdx.math.Vector2;
 import edu.cornell.gdiac.physics.obstacle.ComplexObstacle;
 import edu.cornell.gdiac.physics.obstacle.Obstacle;
 import edu.cornell.gdiac.physics.obstacle.PolygonObstacle;
+import java.util.Random;
 
 
 public class AIController {
@@ -30,6 +31,9 @@ public class AIController {
     private FSMState state;
     private long ticks;
     private Vector2 lineToPlayer;
+    private Vector2 direction;
+
+    Random random = new Random();
 
     private static final float CHASE_RADIUS = 150f;
 
@@ -45,6 +49,22 @@ public class AIController {
 		state = FSMState.WANDER;
 		ticks = 0;
         lineToPlayer = new Vector2();
+        direction = new Vector2();
+	}
+
+	public void update() {
+		updateLineToPlayer();
+		updateFSMState();
+		updateDirectionBasedOnState();
+	}
+
+	/**
+	 * Returns the direction which the controlled enemy should move.
+	 *
+	 * @return The direction that the enemy should move.
+	 */
+	public Vector2 getMovementDirection() {
+		return direction;
 	}
 
     private void updateLineToPlayer() {
@@ -52,17 +72,49 @@ public class AIController {
         lineToPlayer.sub(controlledEnemy.getPosition());
     }
 
-    public void update() {
-
-	}
-
 	private void updateFSMState() {
-//		switch (this.state) {
-//			case WANDER:
-//				if ()
-//		}
+		float distanceToPlayer = controlledEnemy.getPosition().dst(levelModel.getAvatar().getPosition());
+		switch (this.state) {
+			case WANDER:
+				if (distanceToPlayer < CHASE_RADIUS) {
+					this.state = FSMState.CHASE;
+				}
+				break;
+			case CHASE:
+				if (distanceToPlayer > CHASE_RADIUS) {
+					this.state = FSMState.WANDER;
+				}
+		}
 	}
 
+	private void updateDirectionBasedOnState() {
+		switch (this.state) {
+			case WANDER:
+				setDirectionToRandomDirection();
+				break;
+
+			case CHASE:
+				setDirectionToGoTowardsPlayer();
+				break;
+		}
+	}
+
+	/**
+	 * Sets the direction vector to be a random direction.
+	 */
+	private void setDirectionToRandomDirection() {
+		int angle = random.nextInt(360);
+		direction.setAngleDeg(angle);
+		direction.nor();
+	}
+
+	/**
+	 * Set the direction vector to go towards the player.
+	 */
+	private void setDirectionToGoTowardsPlayer() {
+		direction.set(lineToPlayer);
+		direction.nor();
+	}
 
     private boolean isLineCollidingWithAPlatform() {
 		PlatformModel platform = levelModel.getPlatforms();
@@ -71,4 +123,5 @@ public class AIController {
 		}
 		return false;
     }
+
 }
